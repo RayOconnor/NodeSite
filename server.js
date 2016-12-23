865e11b7be5d8fb382544cb5825dcdb7c9ab1ae0
RayOconnor <roconnorc@gmail.com>
2016-12-23 12:50:49 -0500

    wire up css file for custom styles

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
-var express = require('express');
+var express = require("express");
 var app = express();
+var router = express.Router();
+var path = __dirname + '/views/';
 
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 8080;
-
-// set the view engine to ejs
-app.set('view engine', 'ejs');
-
-// make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
 
-// set the home page route
-app.get('/', function(req, res) {
-
-    // ejs render automatically looks in the views folder
-    res.render('index');
+router.use(function (req,res,next) {
+  console.log("/" + req.method);
+  next();
 });
 
-app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
+router.get("/",function(req,res){
+  res.sendFile(path + "index.html");
+});
+
+app.use("/",router);
+
+app.use("*",function(req,res){
+  res.sendFile(path + "404.html");
+});
+
+app.listen(3000,function(){
+  console.log("Live at Port 3000");
 });
